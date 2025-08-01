24e1da696a60609b1d70ca23fa983c9045e0b023
Aldahir Infante <aldahirinfante66@gmail.com>
2025-08-01 01:49:21 -0500

    Corrijo vite.config.js para que encuentre index.html

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
 export default defineConfig({
   plugins: [react()],
+  root: '.', // asegura que busque en la raíz del proyecto
+  build: {
+    rollupOptions: {
+      input: resolve(__dirname, 'public/index.html'),
+    },
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
 });
